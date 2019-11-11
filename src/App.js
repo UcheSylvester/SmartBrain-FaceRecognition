0461f7b7c35a3e45936e6813a6ab8ec3bf44b0cb
UcheSylvester <okorocode@gmail.com>
2019-11-11 21:25:52 +0100

    building functionalities

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Navigation from './components/Navigation/Navigation'
@@ -6,18 +6,39 @@ import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <div className="body-container">
-        <Rank />
-        <ImageLinkForm />
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonSubmit = () => {
+    console.log('clicked')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <div className="body-container">
+          <Rank />
+          <ImageLinkForm
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+          />
+        </div>
+        {/*<FaceRecogntion /> */}
       </div>
-      {/*<FaceRecogntion /> */}
-    </div>
-  );
+    );
+  }
 }
+
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
@@ -10,8 +10,14 @@ const ImageLinkForm = () => {
 
       <div className="center">
         <div className="shadow-5 pa4 br3 form center">
-          <input type="text" className="f4 pa2 w-70 center" />
-          <button className="w-30 grow f4 ph3 link pv2 dib white bg-light-purple">Detect</button>
+          <input type="text"
+            className="f4 pa2 w-70 center"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 ph3 link pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}
+          >Detect</button>
         </div>
       </div>
     </div>
