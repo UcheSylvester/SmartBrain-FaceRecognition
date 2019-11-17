f80998b77514efa5bd28ff676cb05ce5dd751413
UcheSylvester <okorocode@gmail.com>
2019-11-17 17:26:58 +0100

    predicting color

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import FaceRecogntion from './components/FaceRecognition/FaceRecognition'
+
+// import Clarifai from 'clarifai'
+
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '95d6002e6c0946308b035aa3fbf0ffee'
+});
 
 class App extends Component {
   constructor() {
@@ -20,21 +29,37 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('clicked')
+
+    app.models.predict(
+      Clarifai.COLOR_MODEL,
+      "http://bakebookey.com/wp-content/uploads/2019/01/download.jpg")
+      .then(
+        (response) => {
+          console.log('response', response)
+        },
+        (error) => {
+          console.log('error', error)
+        }
+      )
   }
 
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
+        <div className="row">
+          <Logo />
+          <Navigation />
+        </div>
+
         <div className="body-container">
           <Rank />
           <ImageLinkForm
             onInputChange={this.onInputChange}
             onButtonSubmit={this.onButtonSubmit}
           />
+
+          <FaceRecogntion />
         </div>
-        {/*<FaceRecogntion /> */}
       </div>
     );
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const FaceRecognition = () => {
+  return (
+    <div className="center" style={{ marginTop: "1em" }}>
+      <img src="http://bakebookey.com/wp-content/uploads/2019/01/download.jpg" alt="detected" />
+    </div>
+  )
+}
+
+export default FaceRecognition
