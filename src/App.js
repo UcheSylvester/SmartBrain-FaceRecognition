751b6a3c6187166ac1c0ac047d17da8e26c18306
UcheSylvester <okorocode@gmail.com>
2019-11-17 19:27:13 +0100

    detecting face sucessfully!

--- src/App.js
+++ src/App.js
@@ -19,28 +19,50 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      input: ''
+      input: '',
+      imageUrl: '',
+      box: ''
     }
   }
 
+  calculateFaceLocation = (data) => {
+    const faceRegion = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('input-image')
+    const width = image.width;
+    const height = image.height;
+
+    // console.log(faceRegion, width, height)
+
+    return {
+      leftCol: faceRegion.left_col * width,
+      topRow: faceRegion.top_row * height,
+      rightCol: width - (faceRegion.right_col * width),
+      bottomRow: height - (faceRegion.bottom_row * height)
+    }
+  }
+
+  setFaceBox = (box) => {
+    console.log(box)
+    this.setState({ box: box })
+  }
+
   onInputChange = (event) => {
-    console.log(event.target.value)
+    this.setState({
+      input: event.target.value
+    })
   }
 
   onButtonSubmit = () => {
     console.log('clicked')
+    this.setState({
+      imageUrl: this.state.input
+    })
 
     app.models.predict(
-      Clarifai.COLOR_MODEL,
-      "http://bakebookey.com/wp-content/uploads/2019/01/download.jpg")
-      .then(
-        (response) => {
-          console.log('response', response)
-        },
-        (error) => {
-          console.log('error', error)
-        }
-      )
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.setFaceBox(this.calculateFaceLocation(response)))
+      .catch(error => console.log(error))
   }
 
   render() {
@@ -52,14 +74,16 @@ class App extends Component {
         </div>
 
         <div className="body-container">
-          <Rank />
-          <ImageLinkForm
-            onInputChange={this.onInputChange}
-            onButtonSubmit={this.onButtonSubmit}
-          />
+        <Rank />
 
-          <FaceRecogntion />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecogntion box={this.state.box} imageUrl={this.state.imageUrl} />
+
         </div>
+
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './FaceRecognition.css'
 
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ box, imageUrl }) => {
   return (
-    <div className="center" style={{ marginTop: "1em" }}>
-      <img src="http://bakebookey.com/wp-content/uploads/2019/01/download.jpg" alt="detected" />
+    <div className="center">
+      <div className="absolute" style={{ top: '425px' }}>
+        <img id="input-image" src={imageUrl} alt="" width="500px" height="auto" />
+        <div id="bounding-box" style={{
+          top: box.topRow,
+          right: box.rightCol,
+          bottom: box.bottomRow,
+          left: box.leftCol
+        }}></div>
+      </div>
     </div>
   )
 }
